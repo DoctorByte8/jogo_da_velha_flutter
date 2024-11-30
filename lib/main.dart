d3e20fbffd970d85588ddcafc83d7a993876617d
Thiago Aragão <thiago.alpha.06@gmail.com>
2024-11-30 13:46:26 -0300

    Campos de nomes
    Inclusão dos TextField (desta vez, funcionando) para inserir os nomes, e início de layout.

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,55 @@ void main() {
     MaterialApp(
       title: "Jogo da Velha",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue)
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MainApp(),
     )
   );
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  TextEditingController nomeJogador1 = TextEditingController();
+  TextEditingController nomeJogador2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Jogo da Velha",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('Bem vindo!',
+              style: TextStyle(
+                fontSize: 20.0
+              )
+            ),
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(labelText: "Nome do jogador 1"),
+              enabled: true,
+              maxLength: 20,
+              controller: nomeJogador1,
+            ),
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(labelText: "Nome do jogador 2"),
+              enabled: true,
+              maxLength: 20,
+              controller: nomeJogador2,
+            ),
+          ],
         ),
       ),
     );
